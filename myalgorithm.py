e8c8058822e4f9ca1efb334cf41e1b08b218dcb3
Akmaral <47251442+Akmaralka@users.noreply.github.com>
2019-04-26 06:25:59 +0600

    Update myalgorithm.py

--- myalgorithm.py
+++ myalgorithm.py
@@ -1,25 +1,25 @@
 def kruskals_algorithm(graph):
     result = []
-    i = 0
-    e = 0
-    new_graph = sorted(graph.edges(), key=lambda item: item[2])
-    parent_vertex = []
-    rank = []
-    for vertex in range(graph.num_vertices()):
-        parent_vertex.append(vertex)
-        rank.append(0)
+    ind_graph = 0
+    result_count = 0
 
-    while e < graph.num_vertices() - 1:
+    sorted_edges = sorted(graph.edges(), key=lambda part: part[2])
+    parent_vertex = [vertex for vertex in range(graph.num_vertices())]
+    rank = [i for i in range(graph.num_vertices())]
 
-        u, v, w = new_graph[i]
-        i = i + 1
-        a = graph.find(parent_vertex, u)
-        z = graph.find(parent_vertex, v)
+    while result_count < graph.num_vertices() - 1:
+        if ind_graph >= len(sorted_edges):
+            break
 
-        if a != z:
-            e = e + 1
+        u, v, w = sorted_edges[ind_graph]
+        first = graph.find(parent_vertex, u)
+        last = graph.find(parent_vertex, v)
+        ind_graph += 1
+
+        if first != last:
+            result_count = result_count + 1
             result.append([u, v, w])
-            graph.union(parent_vertex, rank, a, z)
+            graph.union(parent_vertex, rank, first, last)
 
     for u, v, weight in result:
         print(str(u) + " -- " + str(v) + " == " + str(weight))
